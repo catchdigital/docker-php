d60d9c9446e629a777eb8c0542a6288b424d6eb0
Liam Hiscock <l.hiscock@catchdigital.com>
2018-03-16 14:15:09 +0000

    Put images back to using fpm

--- update.sh
+++ update.sh
@@ -9,7 +9,7 @@ cp -rf $version/install.sh $version/dev/install.sh
 cp -rf $version/install.sh $version/prod/install.sh
 }
 
-versions=(5.6 7.1 7.2)
+versions=(5.6-fpm 7.1-fpm 7.2-fpm)
 for version in ${versions[*]}; do
   render Dockerfile-dev.template $version/dev/Dockerfile
   render Dockerfile.template $version/prod/Dockerfile
