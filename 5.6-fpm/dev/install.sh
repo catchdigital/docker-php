113e4aac45bfa651adec78074ac106e32068bb51
Liam Hiscock <l.hiscock@catchdigital.com>
2018-03-16 16:03:14 +0000

    Added xdebug support back in correctly

--- 5.6-fpm/dev/install.sh
+++ 5.6-fpm/dev/install.sh
@@ -3,3 +3,9 @@
 apt-get -y install libz-dev libmemcached-dev libmemcached11 libmemcachedutil2 build-essential
 pecl install memcached-2.2.0
 echo extension=memcached.so >> /usr/local/etc/php/conf.d/memcached.ini
+
+
+if [ ! -z $1 ] && [ $1="dev" ]; then
+   pecl install xdebug-2.5.5
+   docker-php-ext-enable xdebug
+fi
--- 7.1-fpm/install.sh
+++ 7.1-fpm/install.sh
@@ -4,3 +4,8 @@
 apt-get install libmemcached-dev -y
 pecl install memcached
 docker-php-ext-enable memcached
+
+if [ ! -z $1 ] && [ $1="dev" ]; then
+   pecl install xdebug
+   docker-php-ext-enable xdebug
+fi
